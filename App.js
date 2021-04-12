af3c2a4648f84342dbd889aaaa9f24d6df1806da
farhadamrah <emferhad17631@sabah.edu.az>
2021-04-12 21:07:09 +0400

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+    Keyboard.dismiss();
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'android'}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={'Write a task'}
+          value={task}
+          onChangeText={(text) => {
+            setTask(text);
+          }}
+        />
+
+        <TouchableOpacity
+          onPress={() => {
+            if (!task) {
+              return;
+            }
+            handleAddTask();
+          }}
+        >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+      <View style={styles.border} />
+
+      <View style={styles.tasksWrapper}>
+        <ScrollView>
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <View style={styles.items} key={Math.random()}>
+                  <View style={styles.circle}></View>
+                  <Text style={styles.itemText}>{item}</Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -14,8 +82,83 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4419EB',
+    overflow: 'hidden',
+    display: 'flex',
+  },
+  tasksWrapper: {
+    // paddingTop: 150,
+    marginVertical: 10,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    letterSpacing: 1.5,
+    marginVertical: 40,
+    // marginBottom: 0,
+  },
+  items: {
+    width: '100%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 15,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
     alignItems: 'center',
+    marginBottom: 17,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  circle: {
+    width: 14,
+    height: 14,
+    borderColor: '#4419EB',
+    borderRadius: 50,
+    borderWidth: 3.5,
+    marginRight: 8,
+  },
+
+  writeTaskWrapper: {
+    // position: 'absolute',
+    // top: 100,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    backgroundColor: 'white',
+    width: 270,
+    padding: 12,
+    borderRadius: 60,
+    borderWidth: 1,
+    borderColor: '#C0C0C0',
+    fontSize: 20,
+  },
+  addWrapper: {
+    height: 55,
+    width: 55,
+    backgroundColor: 'white',
+    borderRadius: 60,
     justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#C0C0C0',
+  },
+  addText: {
+    fontSize: 25,
+  },
+  border: {
+    width: '90%',
+    height: 1,
+    backgroundColor: '#C1C1C1',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginVertical: 30,
   },
 });
